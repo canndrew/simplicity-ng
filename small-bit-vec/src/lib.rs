60097534b7635b01f85b8588e33db3f877711dba
Andrew Poelstra <apoelstra@wpsoftware.net>
2026-08-14 16:41:08 +0000

    small_bit_vec: get compiling on stable Rust
    This crate now compiles on Rust 1.93.0. By replacing the use of
    unchecked_shl we can get the MSRV down to 1.85; to go further we need
    significant refactors to remove Rust 2024 syntax features.
    
    For now let's target 1.93 and we'll see where we get.

--- small-bit-vec/src/lib.rs
+++ small-bit-vec/src/lib.rs
@@ -1,10 +1,25 @@
-#![feature(disjoint_bitor)]
-
 mod small_bit_vec;
 mod bmi2;
 
+use core::hint::unreachable_unchecked;
 pub use self::small_bit_vec::{SmallBitVec, Iter, IntoIter};
 
 #[cfg(test)]
 mod test;
 
+/// Takes the bitwise `or` of two 32-bit values whose 1 bits do not overlap.
+/// 
+/// For such values, `|` and `+` are equivalent, and the compiler may use this to its advantage.
+///
+/// Rust stdlib tracking issue: https://github.com/rust-lang/rust/issues/135758
+///
+/// # Safety
+///
+/// If any bits are set in both `a` and `b`, the result is undefined behavior.
+unsafe fn unchecked_disjoint_bitor(a: usize, b: usize) -> usize {
+    if a & b != 0 {
+        // SAFETY:
+        unsafe { unreachable_unchecked() }
+    }
+    a | b
+}
